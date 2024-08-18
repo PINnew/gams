100dcc35c7837a4cb23072deea7e131ec3d81106
PINnew <pinnew@ya.ru>
2024-08-18 17:24:12 +0400

    add new score

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import random
 pygame.init()
 
+#Экран
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -10,6 +11,7 @@ pygame.display.set_caption('Игра Тир')
 icon = pygame.image.load('img/Тир.jpg')
 pygame.display.set_icon(icon)
 
+#Мишень
 target_img = pygame.image.load('img/targe.png')
 target_width = 50
 target_height = 50
@@ -17,7 +19,12 @@ target_height = 50
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+#Случайный фон
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+score = 0
+
+font = pygame.font.Font(None, 36)
 
 running = True
 while running:
@@ -28,9 +35,15 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                score += 1
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     screen.blit(target_img, (target_x, target_y))
+
+#Отображение Очки
+    score_text = font.render(f'Очки: {score}', True, (255, 255, 255))
+    screen.blit(score_text, (SCREEN_WIDTH - score_text.get_width() - 10, 10))
+
     pygame.display.update()
 
 pygame.quit()
